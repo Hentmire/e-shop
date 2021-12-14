f7f7046982d3d5b92d71d2937d5d7acec184f803
Victoria Udodava <hentmire@gmail.com>
2021-12-14 23:39:15 +0300

    Add user routes and authentication

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -1,29 +1,143 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const { User } = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.get("/", async (req, res) => {
-    const userList = await User.find();
-    if (!userList) {
-        res.status(500).json({
-            success: false,
-        });
-    }
-    res.send(userList);
-});
-
-router.post("/", (req, res) => {
-    const user = new User(req.body);
-    user.save()
-        .then((createdUser) => {
-            res.status(201).json(createdUser);
-        })
-        .catch((err) => {
+    try {
+        const userList = await User.find().select("-passwordHash");
+        if (!userList) {
             res.status(500).json({
-                error: err,
                 success: false,
             });
+        }
+        res.send(userList);
+    } catch (e) {
+        console.log("ERROR GET ALL USERS", e);
+        res.status(500).json({
+            success: false,
+            error: e,
         });
+    }
+});
+
+router.get("/:userId", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId).select(
+            "-passwordHash",
+        );
+
+        if (!user) {
+            res.status(500).json({
+                success: false,
+                message: "The category with the given ID was not found",
+            });
+        }
+
+        res.status(200).send(user);
+    } catch (e) {
+        console.log("ERROR GET A USER", e);
+        res.status(500).json({
+            success: false,
+            error: e,
+        });
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        let user = new User({
+            ...req.body,
+            passwordHash: bcrypt.hashSync(req.body.password, 10),
+        });
+        user = await user.save();
+
+        if (!user) {
+            return res.status(400).send("The user cannot be created.");
+        }
+        res.send(user);
+    } catch (e) {
+        console.log("ERROR POST USER", e);
+        res.status(500).json({
+            success: false,
+            error: e,
+        });
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        const secret = process.env.secret;
+
+        if (!user) {
+            return res.status(400).send("The user are not found");
+        }
+
+        if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+            const token = jwt.sign(
+                {
+                    userId: user.id,
+                    isAdmin: user.isAdmin,
+                },
+                secret,
+                { expiresIn: "1d" },
+            );
+
+            res.status(200).send({ user: user.email, token });
+        } else {
+            res.status(400).send("Password is wrong");
+        }
+    } catch (e) {
+        console.log("ERROR LOGIN USER", e);
+        res.status(500).json({
+            success: false,
+            error: e,
+        });
+    }
+});
+
+router.post("/register", async (req, res) => {
+    try {
+        let user = new User({
+            ...req.body,
+            passwordHash: bcrypt.hashSync(req.body.password, 10),
+        });
+        user = await user.save();
+
+        if (!user) {
+            return res.status(400).send("The user cannot be created.");
+        }
+        res.send(user);
+    } catch (e) {
+        console.log("ERROR POST USER", e);
+        res.status(500).json({
+            success: false,
+            error: e,
+        });
+    }
+});
+
+router.get("/get/count", async (req, res) => {
+    try {
+        const userCount = await User.countDocuments();
+
+        if (!userCount) {
+            res.status(500).json({
+                success: false,
+            });
+        }
+        res.send({
+            userCount,
+        });
+    } catch (e) {
+        console.log("ERROR GET USER COUNT", e);
+        return res.status(400).json({
+            success: false,
+            error: e,
+        });
+    }
 });
 
 module.exports = router;
